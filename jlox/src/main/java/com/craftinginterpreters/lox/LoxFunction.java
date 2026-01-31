dbe49482eca4b5ef6fc583617696dd217b6f89a3
Jack Jackson <scubbojj@gmail.com>
2026-01-30 23:16:51 -0800

    Chapter 10, Part 3 - closures

--- jlox/src/main/java/com/craftinginterpreters/lox/LoxFunction.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/LoxFunction.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
 
+    private final Environment closure;
     private final Stmt.Function declaration;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
+        this.closure = closure;
         this.declaration = declaration;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(
                 declaration.params.get(i).lexeme,
